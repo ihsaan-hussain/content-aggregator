41d3455a0fb837bf398f2501a555df65cc01d176
Ihsaan Hussain <ihsaan09@outlook.com>
2023-10-09 20:40:31 +0100

    added beautiful soup

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    url = "https://news.google.com/home?hl=en-GB&gl=GB&ceid=GB:en"
+
+    get_url = requests.get(url)
+    get_text = get_url.text
+
+    soup = BeautifulSoup(get_text, "html.parser")
+
+    return render_template('home.html',  tags=tags, soup=soup)
