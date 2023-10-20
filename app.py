d43c022dd2b6601ed52be5259199b93fa3b61552
Ihsaan Hussain <ihsaan09@outlook.com>
2023-10-20 19:16:11 +0100

    added cards

--- app.py
+++ app.py
@@ -11,17 +11,20 @@ def home():
 @app.route('/world')
 def world():
     # world news page
-
     headlines = []
+    images_list = []
 
     url = "https://www.bbc.co.uk/news/world"
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, "lxml")
-    
     h3 = soup.find_all("h3")
+    images = soup.find_all("img", src="https://ichef.bbci.co.uk/news/490/cpsprodpb/E7E8/production/_131486395_gettyimages-1746151111.jpg")
+
+    for image in images:
+        images_list.append(image['src'])
 
     for x in h3:
         headlines.append(x.text)
 
-    return render_template('world.html', headlines=headlines)
+    return render_template('world.html', headlines=headlines, photos=images_list)
